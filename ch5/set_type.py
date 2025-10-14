53c77d6362ebeaa0e9024a4678b4d497491ae753
nageena88 <ram.nageena88@hotmail.com.com>
2025-10-14 18:58:28 +0530

    fix: added all data types

---
+++ ch5/set_type.py
@@ -0,0 +1,136 @@
+# a = set()
+# print(type(a))
+
+a = {1, 2, 3, 4, 5}
+b = {3, 4, 5, 6, 7}
+# print(a)
+# print(type(a))
+
+# x = a - b
+# print(x)
+
+# x = a | b
+# print(x)
+
+# x = a & b
+# print(x)
+
+# x = a ^ b
+# print(x)
+
+
+# print(dir(a))
+
+methods = [
+    # "add",
+    # "clear",
+    # "copy",
+    # "difference",
+    # "difference_update",
+    # "discard",
+    # "intersection",
+    # "intersection_update",
+    # "isdisjoint",
+    # "issubset",
+    # "issuperset",
+    # "pop",
+    # "remove",
+    # "symmetric_difference",
+    # "symmetric_difference_update",
+    # "union",
+    # "update",
+]
+
+# x = {1, 2}
+# x.add(3)
+# x.add(3)
+# print(x)
+
+
+# res = a.difference(b)
+# print(res)
+# print(a)
+# print(b)
+
+# a.difference_update(b)
+# print(a)
+# print(b)
+
+# discard
+
+# a.discard(3)
+# print(a)
+
+# a.discard(13)
+# print(a)
+
+# res = a.intersection(b)
+# print(res)
+# print(a, b)
+
+
+# a.intersection_update(b)
+# print(a)
+# print(b)
+
+# "pop",
+
+# x = a.pop()
+# print(x)
+# x = a.pop()
+# print(x)
+# print(a)
+
+# "remove",
+
+# a.remove(3)
+# print(a)
+
+# a.remove(13)
+# print(a)
+
+# "symmetric_difference",
+# res = a.symmetric_difference(b)
+# print(res)
+# print(a, b)
+# "symmetric_difference_update",
+# a.symmetric_difference_update(b)
+# print(a)
+# print(b)
+
+
+# "union",
+# res = a.union(b)
+# print(res)
+
+# "update",
+# a.update(b)
+# print(a)
+# print(b)
+
+
+# "isdisjoint",
+# print(a, b)
+
+# res = a.isdisjoint(b)
+# print(res)
+
+# "issubset",
+# "issuperset",
+
+# a = {1, 2, 3, 4, 5, 6, 7}
+# b = {4, 5, 6}
+
+# res = a.issubset(b)
+# print(res)
+
+# res = b.issubset(a)
+# print(res)
+
+# res = a.issuperset(b)
+# print(res)
+
+# res = b.issuperset(a)
+# print(res)
+
+# print(a[0]) # error
